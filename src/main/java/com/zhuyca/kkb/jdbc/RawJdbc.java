4059d37379cecd84e0884750565832ea91d6fad2
zhuyongchang <zhuyongchang@meicai.cn>
2019-08-30 12:24:54 +0800

    raw jdbc

---
+++ src/main/java/com/zhuyca/kkb/jdbc/RawJdbc.java
@@ -0,0 +1,71 @@
+package com.zhuyca.kkb.jdbc;
+
+import java.sql.*;
+
+/**
+ * @Title :
+ * @Program :       rawjdbc
+ * @Description :
+ * @Date :          2019/8/30 11:35
+ * @Author :        Y.C.ZHU
+ * @Company :       北京云杉世界信息技术有限公司<br>
+ * @Copyright :     Copyright (c) 2016<br>
+ */
+public class RawJdbc {
+    public static void main(String[] args) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet rs = null;
+
+        try {
+            // 加载数据库驱动
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // 通过驱动管理类获取数据库连接
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?serverTimezone=UTC&characterEncoding=utf-8", "root", "88888888");
+
+            // 定义sql语句，？标识占位符
+            String sql = "select * from user where name = ?";
+
+            // 获取预处理 statement
+            preparedStatement = connection.prepareStatement(sql);
+
+            // 设置参数，第一个参数为 sql 语句中参数的序号（从1开始），第二个参数为设置的内容
+            preparedStatement.setString(1, "王五");
+
+            // 向数据库发出sql执行查询，查询出结果集
+            rs = preparedStatement.executeQuery();
+
+            // 遍历查询结果集
+            while (rs.next()) {
+                System.out.println(rs.getString("id") + " " + rs.getString("name"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // 释放资源
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
